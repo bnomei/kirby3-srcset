58546d07711a07c1b59abbb1122af71ca031fdb5
Bruno Meilick <b@bnomei.com>
2018-08-03 18:36:46 +0200

    made more compatible to lazysizes
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- snippets/srcset.php
+++ snippets/srcset.php
@@ -1,9 +1,9 @@
 <?php
     // $file, $lazy, $isLazy, $preset, $sources, $img
 ?>
-<picture class="srcset <?= $lazy ?>" data-preset="<?= $preset ?>">
+<picture class="srcset" data-preset="<?= $preset ?>">
     <?php foreach ($sources as $s): ?>
     <source <?= ($isLazy?'data-':'') ?>srcset="<?= $s['srcset'] ?>" type="<?= $s['type'] ?>" />
     <?php endforeach; ?>
-    <?= ($isLazy?'<noscript>':'') ?><img src="<?= $img['src'] ?>" alt="<?= $img['alt'] ?>" /><?= ($isLazy?'</noscript>':'') ?>
+    <img class="<?= $lazy ?>" <?= ($isLazy?'data-':'') ?>src="<?= $img['src'] ?>" alt="<?= $img['alt'] ?>" />
 </picture>
