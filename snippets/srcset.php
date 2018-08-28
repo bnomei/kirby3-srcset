28de4dc3a9fe20d11164908caa29a4a93daafb49
Bruno Meilick <b@bnomei.com>
2018-08-28 15:54:05 +0200

    improved snippet for kirby tag compability
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- snippets/srcset.php
+++ snippets/srcset.php
@@ -1,9 +1,4 @@
 <?php
     // $file, $lazy, $isLazy, $preset, $sources, $img
 ?>
-<picture class="srcset" data-preset="<?= $preset ?>">
-    <?php foreach ($sources as $s): ?>
-    <source <?= ($isLazy?'data-':'') ?>srcset="<?= $s['srcset'] ?>" type="<?= $s['type'] ?>" />
-    <?php endforeach; ?>
-    <img class="<?= $lazy ?>" <?= ($isLazy?'data-':'') ?>src="<?= $img['src'] ?>" alt="<?= $img['alt'] ?>" />
-</picture>
+<picture class="srcset" data-preset="<?= $preset ?>"><?php foreach ($sources as $s): ?><source <?= ($isLazy?'data-':'') ?>srcset="<?= $s['srcset'] ?>" type="<?= $s['type'] ?>" /><?php endforeach; ?> <img class="<?= $lazy ?>" <?= ($isLazy?'data-':'') ?>src="<?= $img['src'] ?>" alt="<?= $img['alt'] ?>" /></picture>
